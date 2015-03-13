e42a948ce5c029eebd56faa6d91c67caf5df602c
Brian Watson <bmwatson@csu.fullerton.edu>
2015-03-12 17:19:00 -0700

    updates and testing

--- app/Express/server.js
+++ app/Express/server.js
@@ -1,15 +1,49 @@
 var express = require("express"),
 	http = require("http"),
 	app = express();
+	myJson = '{"outcome": "win", "wins": "3"}';
 
 app.use(express.static(__dirname + "/client"));
 
 http.createServer(app).listen(3000);
 
-app.get("/hello", function (req, res) {
-	res.send("Hello World!");
+app.get("/results.json", function (req, res) {
+	res.json({"outcome": "win", "wins": "3"});
+	console.log("this ran");
+	console.log(myJson);
 });
 
-app.get("/goodbye", function (req, res) {
-	res.send("Goodbye World!");
+app.post("/play/rock", function (req, res) {
+	//we got a rock post, so run the function that plays the game
+
+	//once done, return the JSON object back to the page
+	res.json(myJson);
+});
+
+app.post("/play/paper", function (req, res) {
+	//we got a paper post, so run the function that plays the game
+
+	//once done, return the JSON object back to the page
+	res.json(myJson);
+});
+
+app.post("/play/scissors", function (req, res) {
+	//we got a scissors post, so run the function that plays the game
+
+	//once done, return the JSON object back to the page
+	res.json(myJson);
+});
+
+app.post("/play/lizard", function (req, res) {
+	//we got a lizard post, so run the function that plays the game
+
+	//once done, return the JSON object back to the page
+	res.json(myJson);
+});
+
+app.post("/play/spock", function (req, res) {
+	//we got a spock post, so run the function that plays the game
+
+	//once done, return the JSON object back to the page
+	res.json(myJson);
 });
